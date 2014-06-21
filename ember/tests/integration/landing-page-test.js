1e3c99d208a350657e8cb1ec144ace4b9eb3e0f1
Jack Lucky <jack.lucky.iv@gmail.com>
2014-06-21 16:34:55 -0700

    Create static route to "About"

---
+++ ember/tests/integration/landing-page-test.js
@@ -0,0 +1,27 @@
+import startApp from 'triage/tests/helpers/start-app';
+
+var App;
+
+module('Integration - Landing Page', {
+  setup: function() {
+    App = startApp();
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+test('Should welcome me to Triage', function() {
+  visit('/').then(function() {
+    equal(find('h2#title').text(), 'Welcome to Triage');
+  });
+});
+
+test('Should link to the About page', function() {
+  visit('/').then(function() {
+    click("a:contains('About')").then(function() {
+      equal(find('h3').text(), 'About');
+    });
+  });
+});
+
