4b66a4150ec72c84782b894bf924fd6ae5e416a6
Jack Lucky <jack.lucky.iv@gmail.com>
2014-06-21 17:04:54 -0700

    Install Pretender and add to Brocfile

--- ember/Brocfile.js
+++ ember/Brocfile.js
@@ -7,4 +7,8 @@ var app = new EmberApp();
 app.import('vendor/bootstrap/dist/js/bootstrap.js');
 app.import('vendor/bootstrap/dist/css/bootstrap.css');
 
+app.import({development:'vendor/route-recognizer/dist/route-recognizer.js'});
+app.import({development:'vendor/FakeXMLHttpRequest/fake_xml_http_request.js'});
+app.import({development:'vendor/pretender/pretender.js'});
+
 module.exports = app.toTree();
