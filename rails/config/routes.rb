9a5ea97877818a4161f0ea63d2e4afef18a44d1e
Jack Lucky <jack.lucky.iv@gmail.com>
2014-06-22 17:12:09 -0700

    Clean up calendars route for show only

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :speakers
-      resources :calendars
+      resources :calendars, only: [:show]
     end
   end
 end
