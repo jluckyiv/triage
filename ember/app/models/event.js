5aa1e16894fff3648ec4129f37322435770b8856
Jack Lucky <jack.lucky.iv@gmail.com>
2014-06-23 20:42:59 -0700

    Refactor event model

--- ember/app/models/event.js
+++ ember/app/models/event.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   matter:    DS.belongsTo('matter'),
   type:      DS.attr('string'),
-  subject:   DS.attr('string')
+  subject:   DS.attr('string'),
+  action:    DS.attr('string'),
+  timestamp: DS.attr('date')
 });
 
