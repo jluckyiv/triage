1aaff13b5f6f3692a2b70cf3b2f2e2fcfadaaf0c
Jack Lucky <jack.lucky.iv@gmail.com>
2014-06-28 06:44:59 -0700

    Minor refactor calendar

--- ember/app/routes/calendars.js
+++ ember/app/routes/calendars.js
@@ -3,9 +3,37 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function(params) {
-    var date = toUrlDate(params.date);
-    return this.store.find('calendar', date);
-  }
+    this.set('date', toUrlDate(params.date));
+    return this.store.find('calendar', this.get('date'));
+  },
+
+  afterModel: function() {
+    console.log('model = ' + this.get('model'));
+    this.tick();
+  },
+
+  tick: function() {
+    var now = new Date();
+
+    this.setProperties({
+      second: now.getSeconds(),
+      quarterMinute: Math.round(now.getSeconds() / 15),
+      halfMinute: Math.round(now.getSeconds() / 30),
+      minute: now.getMinutes(),
+      hour:   now.getHours()
+    });
+
+    var self = this;
+    setTimeout(function(){ self.tick(); }, 1000);
+  },
+
+  reload: (function() {
+    console.log('reload();');
+    this.modelFor(this.routeName).reload();
+    // this.get('model').reload();
+    // return this.store.find('calendar', this.get('date'));
+  }).observes('quarterMinute')
+
 });
 
 function toUrlDate(dateText) {
