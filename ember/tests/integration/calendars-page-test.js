597f351662c03bf527cd1dc636bf3b49f7864262
Jack Lucky <jack.lucky.iv@gmail.com>
2014-06-28 23:32:35 -0700

    Update tests to match new formatting

--- ember/tests/integration/calendars-page-test.js
+++ ember/tests/integration/calendars-page-test.js
@@ -18,7 +18,9 @@ module('Integration - Calendars Page', {
 test('Should navigate to the Calendars page', function() {
   var text = "Triage Calendar for June 23, 2014";
   visit('/calendars/20140623').then(function() {
-    equal(find('#date').text(), text);
+    equal(find('#date').text().indexOf("Triage Calendar"), 0);
+    equal(find('#date').text().indexOf("All cases") , 27);
+    equal(find('#date').text().indexOf("June 23, 2014"), 47);
   });
 });
 
@@ -27,7 +29,9 @@ test('Should navigate to the date typed into the date form', function() {
   visit('/').then(function() {
     fillIn('#date-input', '6/24/14');
     click('#date-submit').then(function() {
-      equal(find('#date').text(), text);
+    equal(find('#date').text().indexOf("Triage Calendar"), 0);
+    equal(find('#date').text().indexOf("All cases"), 27);
+    equal(find('#date').text().indexOf("June 24, 2014"), 47);
     });
   });
 });
