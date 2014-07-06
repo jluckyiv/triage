112cd272025c6261442e6bd19c401286f5198db8
Jack Lucky <jack.lucky.iv@gmail.com>
2014-07-06 00:49:34 -0400

    Eliminate calendar from fake server response

--- ember/tests/helpers/pretend-calendar-server.js
+++ ember/tests/helpers/pretend-calendar-server.js
@@ -1,15 +1,11 @@
 /* global require */
 
 export default function pretendCalendarServer(attrs) {
-  var Server, pretend200, calendarForId, calendars, matters, events;
+  var Server, pretend200, mattersForDate, matters, events;
   Server = new Pretender(function() {
 
-    this.get('/api/v1/calendars/:id', function(request) {
-      return (calendarForId(request.params.id));
-    });
-
     this.get('/api/v1/matters', function(request) {
-      return (calendarForId(request.queryParams.calendar));
+      return (mattersForDate(request.queryParams.date));
     });
 
   });
@@ -18,17 +14,11 @@ export default function pretendCalendarServer(attrs) {
     return [200, {"Content-Type": "application/json"}, JSON.stringify(data)];
   };
 
-   calendarForId = function(id) {
-    var calendar, calendarMatters;
-
-    calendar = calendars.find(function(calendar) {
-      if (calendar.date === Date.parse(id).toString("yyyyMMdd")) {
-        return calendar;
-      }
-    });
+   mattersForDate = function(date) {
+    var calendarMatters;
 
     calendarMatters = matters.filter(function(matter) {
-      if (matter.calendar_id === calendar.id) {
+      if (matter.date === Date.parse(date).toString("yyyyMMdd")) {
         matter.events = events.filter(function(event) {
           if (event.matter_id === matter.id) {
             return event;
@@ -38,22 +28,16 @@ export default function pretendCalendarServer(attrs) {
       }
     });
 
-    return pretend200({calendar: calendar, matters: calendarMatters});
+    return pretend200({matters: calendarMatters});
   };
 
-   calendars = [
-    { id: 1, date: '20140623', matter_ids: [1,2,3,6] },
-    { id: 2, date: '20140624', matter_ids: [4] },
-    { id: 3, date: '20140630', matter_ids: [5,6] }
-  ];
-
    matters = [
-    { id: 1, calendar_id: 1, department: "F201", case_number: "RIK2100001", petitioner: "KATHRYN HOLT", petitioner_present: true, respondent: "HARVEY STEVENS", respondent_present: true, current_station: "Triage", checked_in: true, event_ids: [1,2] },
-    { id: 2, calendar_id: 1, department: "F301", case_number: "RIK2100002", petitioner: "COURTNEY HOLLAND", petitioner_present: true, respondent: "WENDELL PARKS", current_station: "Self-help" },
-    { id: 3, calendar_id: 1, department: "F401", case_number: "RID2100003", petitioner: "PAM BLACK", respondent: "ANDREW FRANK", current_station: "Self-help" },
-    { id: 4, calendar_id: 2, department: "F402", case_number: "RID2100004", petitioner: "BRETT HOWELL", respondent: "JOANNE PERKINS", current_station: "Self-help" },
-    { id: 5, calendar_id: 3, department: "F402", case_number: "RID2100005", petitioner: "DUANE WOLFE", respondent: "MARIE HILL", current_station: "Self-help" },
-    { id: 6, calendar_id: 1, department: "F201", case_number: "RID2100006", petitioner: "EDNA WADE", respondent: "WAYNE ANDERSON", current_station: "Self-help" }
+    { id: 1, date: '20140623', department: "F201", case_number: "RIK2100001", petitioner: "KATHRYN HOLT", petitioner_present: true, respondent: "HARVEY STEVENS", respondent_present: true, current_station: "Triage", checked_in: true, event_ids: [1,2] },
+    { id: 2, date: '20140623', department: "F301", case_number: "RIK2100002", petitioner: "COURTNEY HOLLAND", petitioner_present: true, respondent: "WENDELL PARKS", current_station: "Self-help" },
+    { id: 3, date: '20140623', department: "F401", case_number: "RID2100003", petitioner: "PAM BLACK", respondent: "ANDREW FRANK", current_station: "Self-help" },
+    { id: 4, date: '20140623', department: "F201", case_number: "RID2100006", petitioner: "EDNA WADE", respondent: "WAYNE ANDERSON", current_station: "Self-help" },
+    { id: 5, date: '20140624', department: "F402", case_number: "RID2100004", petitioner: "BRETT HOWELL", respondent: "JOANNE PERKINS", current_station: "Self-help" },
+    { id: 6, date: '20140630', department: "F402", case_number: "RID2100005", petitioner: "DUANE WOLFE", respondent: "MARIE HILL", current_station: "Self-help" },
   ];
 
    events = [
