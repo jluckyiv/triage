a41adbd134eec6c074fd71e2f410beff8164fa97
Jack Lucky <jack.lucky.iv@gmail.com>
2014-07-19 09:35:39 -0700

    Remove speakers route

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :speakers
       resources :calendars, only: [:show]
       resources :matters, only: [:index]
       resources :events, only: [:create]
