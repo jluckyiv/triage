cd0be52b5a0e9653af1828c634de626b4f1e5181
Jack Lucky <jack.lucky.iv@gmail.com>
2014-07-29 04:53:47 -0700

    Eliminate speakers routes

--- ember/app/router.js
+++ ember/app/router.js
@@ -6,9 +6,6 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
-  this.resource('speakers', function() {
-    this.route('show', {path: ':speaker_id'});
-  });
   this.resource('calendars', {path: '/calendars/:date'});
   this.resource('matters', {path: '/matters/:date'});
 });
